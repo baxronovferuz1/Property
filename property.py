1c893699c1d9452aa4622bbd2e0bc65ffb6569b0
baxronovferuz1 <baxronovvferuz25@gmail.com>
2022-11-05 22:33:38 +0500

    addition

--- property.py
+++ property.py
@@ -68,3 +68,53 @@ urls =[
 for url in urls:
     page=WebPage(url)
     print(f'{url}\tsize={format(page.page_size, "_")}\telapsed={page.time_elapsed: .2f} secs')
+
+
+
+
+#ADDITION
+
+
+
+from numbers import Real
+
+class Vector:
+    def __init__(self, *components):
+        if len(components)<1:
+            raise ValueError("cannot create an empty vector")
+        for component in components:
+            if not isinstance(component, Real):
+                raise ValueError(f"vector components must all be real numbers. {component} is invalid")
+
+        self._components=tuple(components)
+
+
+    def __len__(self):
+        return len(self._components)
+
+
+    @property
+    def components(self):
+        return self._components
+
+    def __repr__(self):
+        return f"vector ({self.components})"
+
+
+    def confirm_type_and_size(self,c):
+        return isinstance(c,Vector) and len(c)==len(self)
+
+    def __add__(self,other):
+        if not self.confirm_type_and_size(other):
+            return NotImplemented
+        components=(x+y for x,y in zip(self.components, other.components))
+        return Vector(*components)
+
+ 
+
+
+    def __mul__(self,other):
+        if not isinstance(other,Real):
+            return NotImplemented
+        components=(other * x for x in self.components)
+        return Vector(*components)
